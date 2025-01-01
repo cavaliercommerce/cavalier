1369d08ec2fb05aa9e0b4594bf32b8fa083f0899
Bart Stefanski <contact@bstefanski.com>
2025-01-01 23:22:34 +0100

    chore: manual deployment option & add test step before & resuse nodejs version

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+void 0;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 8080);
