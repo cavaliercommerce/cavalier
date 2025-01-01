5074215a177f22dc369e218fd9be1caff9bbcd73
Bart Stefanski <contact@bstefanski.com>
2025-01-01 22:25:04 +0100

    feat: add zod validation pipe

--- apps/gateway-service/src/app.module.ts
+++ apps/gateway-service/src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { APP_PIPE } from "@nestjs/core";
+import { ZodValidationPipe } from "nestjs-zod";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
