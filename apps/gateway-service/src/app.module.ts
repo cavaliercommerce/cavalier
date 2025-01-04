e3ad31f47271c55dd4036cfcbde509f41ed9c40a
Bart Stefanski <contact@bstefanski.com>
2025-01-04 01:19:41 +0100

    feat: add dotenv & lazily load prisma to test db connection

--- apps/gateway-service/src/app.module.ts
+++ apps/gateway-service/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { APP_PIPE } from "@nestjs/core";
 import { ZodValidationPipe } from "nestjs-zod";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [
     AppService,
